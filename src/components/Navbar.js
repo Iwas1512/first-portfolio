3895204b9aa175a2da75070afef61b87e1627533
Iwas1512 <anliliwas1@gmail.com$ git config --global user.name>
2025-02-27 17:59:58 -0600

    First draft

---
+++ src/components/Navbar.js
@@ -0,0 +1,54 @@
+import { Link } from "react-router-dom";
+import "./NavbarStyles.css";
+
+
+import React, {useState} from "react";
+
+import { FaBars, FaTimes } from "react-icons/fa";
+
+
+
+
+const Navbar = () => {
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
+
+
+
+
+
+
+  return (
+   < div className="header">
+    <Link to = {"/"}>
+    <h1> Anlil's Portfolio  </h1>
+    
+     </Link>
+     <u1 className = "nav-menu"> 
+        <li>
+            <Link to = "/">Home</Link>
+        </li>
+        <li>
+            <Link to = "/projects">Projects</Link>
+        </li>
+        <li>
+            <Link to = "/about">About</Link>
+        </li>
+        <li>
+            <Link to = "/contact">Contact</Link>
+        </li>
+
+
+     </u1>
+
+     <div className = "hamburger">
+        <FaTimes size={20} style={{color:"fff"}} />
+        <FaBars size={20} style={{color:"fff"}} />
+     </div>
+
+    </div>
+  )
+}
+
+export default Navbar
