bb857ffa701ead3715324b0ff73b01c86b78c6a8
Iwas1512 <anliliwas1@gmail.com$ git config --global user.name>
2025-03-01 12:07:33 -0600

    Added a footer, made navbar appear in every folder.

--- src/routes/Projects.js
+++ src/routes/Projects.js
@@ -1,8 +1,19 @@
 import React from 'react'
 
+import Navbar from '../components/Navbar'
+import Footer from '../components/Footer'
+import HeroImg2 from '../components/HeroImg2'
+
+
+
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div>
+      <Navbar/>
+      <HeroImg2 heading = "PROJECTS (Coming Soon)"text = "My Featured Projects:"/>
+      <Footer/>
+    
+    </div>
   )
 }
 
